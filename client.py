a531f91c80ecdb6ac4120f77c918f284bcd62ca7
Conqu3red <46159994+Conqu3red@users.noreply.github.com>
2022-07-19 14:52:03 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -1,55 +1,128 @@
 import socket
 import threading
-import socketserver
+import json
+import os
+import dh
+
+def enc_json(data) -> bytes:
+    return json.dumps(data).encode("utf-8")
+
+
+def dec_json(data: bytes):
+    print(f"data: {data.decode('utf-8')}")
+    return json.loads(data.decode("utf-8"))
 
 def recv_all(sock: socket.socket, block_size: int = 1024) -> bytes:
-        data = b""
-        while True:
-            new = sock.recv(block_size)
-            data += new
-            if len(new) < block_size:
-                break
-        
-        return data
+    data = b""
+    while True:
+        new = sock.recv(block_size)
+        data += new
+        if len(new) < block_size:
+            break
+
+    return data
+
 
 def client(ip: str, port: int, username: str):
+    DATA_FILE = f"{username}_keys.json"
+
+    try:
+        with open(DATA_FILE, "r") as f:
+            key_data = json.load(f)
+            print("Loaded existing key.")
+    except (OSError, json.JSONDecodeError) as e:
+        print(e)
+        me = dh.DiffieHellman()
+        key_data = {
+            "mine": me.save(),
+            "negotiations": []
+        }
+
+    me = dh.DiffieHellman.load(key_data["mine"])
+
+    with open(DATA_FILE, "w") as f:
+        json.dump(key_data, f, indent=2)
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((ip, port))
-        sock.send(bytes(username, 'utf-8'))
+        sock.send(enc_json({
+            "type": "client_init",
+            "username": username,
+            "public_key": me.gen_public_key()
+        }))
 
         print("Connected.")
 
         stop_event = threading.Event()
-        
+
         def listener():
             try:
                 while not stop_event.is_set():
-                    message = str(recv_all(sock), "utf-8")
-                    print("\n" + message)
+                    data = dec_json(recv_all(sock))
+                    if data["type"] == "message_forward":
+                        print(f"<{data['name']}> {data['message']}\n")
+                    elif data["type"] == "user_connect":
+                        print(f"[+] {data['name']} has connected.")
+                    elif data["type"] == "user_disconnect":
+                        print(f"[-] {data['name']} has disconnected.")
+                    elif data["type"] == "server_close":
+                        print(f"Server closed.")
+                    elif data["type"] == "user_list":
+                        print(f"Users connected: {data['users']}")
+                    elif data["type"] == "disconnect":
+                        print(f"Disconnected from server, reason: {data['reason']}")
+                    elif data["type"] == "conversation_init":
+                        print(f"conversation {data}")
+                        other_name = data["name"]
+                        other_public_key = data["public_key"]
+                        shared_key = me.gen_shared_key(other_public_key)
+                        print(f"Shared key: {shared_key}")
+                    else:
+                        print(f"Unimplemented message type {data['type']}")
+
             except Exception as e:
+                print("err", repr(e))
                 if stop_event.is_set():
                     print("Disconnected, closing...")
                 else:
                     print("Lost connection, closing...")
                 stop_event.set()
-        
+
         t = threading.Thread(target=listener)
         t.daemon = True
         t.start()
 
+        target = input("Who to message?")
+        sock.sendall(enc_json({
+            "type": "conversation_request",
+            "name": target
+        }))
+
         try:
             while not stop_event.is_set():
                 message = input("")
-                sock.sendall(bytes(message, 'utf-8'))
+                sock.sendall(enc_json({
+                    "type": "message",
+                    "message": message
+                }))
         except KeyboardInterrupt:
             stop_event.set()
+            sock.close()
         except:
             pass
 
+my_server = True
 
-ip = "127.0.0.1"
-port = 6777
+if my_server:
+    ip = "172.16.44.141"
+    port = 6777
+else:
+    ip = "172.16.55.127"
+    port = 55555
+
+# my server: 172.16.44.141
+# riley's server: 172.16.55.127
+#ip = "172.16.44.141" #socket.gethostbyname(socket.gethostname()) #"127.0.0.1"
+#port = 55555 #6777
 
 client(ip, port, input("Username: "))
-#client(ip, port, "Hello World 2")
-#client(ip, port, "Hello World 3")
---
+++ server_direct.py
@@ -0,0 +1,212 @@
+import socket
+import threading
+import socketserver
+import time
+from typing import *
+from dataclasses import dataclass
+import logging
+import json
+
+DATETIME_FMT = "%Y-%m-%d %H:%M:%S"
+FORMAT = '[%(asctime)s %(levelname)s %(module)s:%(lineno)d] %(message)s'
+logging.basicConfig(format=FORMAT, datefmt=DATETIME_FMT, level=logging.INFO)
+logger = logging.getLogger('tcp_server')
+
+@dataclass
+class Client:
+    username: str
+
+
+def enc_json(data) -> bytes:
+    return json.dumps(data).encode("utf-8")
+
+
+def dec_json(data: bytes):
+    return json.loads(data.decode("utf-8"))
+
+
+def try_send(sock: socket.socket, data) -> bool:
+    try:
+        sock.send(data)
+        return True
+    except Exception as e:
+        logger.error(f"Exception whilst trying to send data: {e}")
+        return False
+
+
+def recv_all(sock: socket.socket, block_size: int = 1024) -> bytes:
+    data = b""
+    while True:
+        new = sock.recv(block_size)
+        data += new
+        if len(new) < block_size:
+            break
+
+    return data
+
+
+class Server:
+    KEY_FILE = "server_keys.json"
+
+    def __init__(self, sock: socket.socket) -> None:
+        self.clients: Dict[socket.socket, Tuple[threading.Thread, Client]] = {}
+        self.server_sock = sock
+        self.public_keys: Dict[str, int] = {}
+
+        try:
+            with open(self.KEY_FILE, "r") as f:
+                self.public_keys = json.load(f)
+
+        except (OSError, json.JSONDecodeError):
+            pass
+
+    def get_sock(self, name: str) -> Optional[socket.socket]:
+        for sock, (_, client) in self.clients.items():
+            if client.username == name:
+                return sock
+
+    def client_link(self, sock: socket.socket, stop_event: threading.Event):
+        """Loop for communicating with a client"""
+        other_sock: Optional[socket.socket] = None
+        other_username: Optional[str] = None
+
+        while not stop_event.is_set():
+            try:
+                data = dec_json(recv_all(sock))
+                if data["type"] == "message":
+                    if other_sock is not None:
+                        client = self.clients[sock][1]
+                        message = data["message"]
+                        other_sock.sendall(enc_json({
+                            "type": "message_forward",
+                            "name": client.username,
+                            "message": message
+                        }))
+
+                elif data["type"] == "conversation_request":
+                    other_username = data["name"]
+                    other_sock = self.get_sock(other_username)
+                    username = self.clients[sock][1].username
+                    if other_sock is not None:
+                        # public key exchange
+                        logger.info(f"Initiating conversation between {username} and {other_username}")
+                        other_sock.sendall(enc_json({
+                            "type": "conversation_init",
+                            "name": username,
+                            "public_key": self.public_keys[username]
+                        }))
+
+                        sock.sendall(enc_json({
+                            "type": "conversation_init",
+                            "name": other_username,
+                            "public_key": self.public_keys[other_username]
+                        }))
+                    else:
+                        # TODO: tell user that user was not found
+                        pass
+
+                else:
+                    logger.error(f"Unimplemented message type {data['type']}")
+
+            except Exception as e:
+                logger.error(f"Error from client: {e!r}")
+                self.disconnect(sock)
+                break
+
+    def disconnect(self, sock: socket.socket):
+        client = self.clients[sock][1]
+        del self.clients[sock]
+        self.send_to_all({
+            "type": "user_disconnect",
+            "name": client.username
+        })
+
+    def send_to_all(self, data):
+        d = enc_json(data)
+        for s in list(self.clients.keys()):
+            success = try_send(s, d)
+            if not success:
+                self.disconnect(s)
+
+    def list_users(self) -> List[str]:
+        return [c.username for _, c in self.clients.values()]
+
+    def loop(self, stop_event: threading.Event):
+        logger.info("Server started")
+        try:
+            while not stop_event.is_set():
+                client_socket, client_address = self.server_sock.accept()
+                logger.debug("Connection")
+
+                initial_data = dec_json(recv_all(client_socket))
+                logger.debug(f"Recieved initial payload: {initial_data}")
+                username = initial_data["username"]
+                public_key = initial_data["public_key"]
+                if username in self.public_keys and self.public_keys[username] != public_key:
+                    logger.info(f"Found mismatching key for user {username}")
+                    client_socket.send(enc_json({
+                        "type": "disconnect",
+                        "reason": f"provided public key does not match for user '{username}'"
+                    }))
+                    continue
+                else:
+                    self.public_keys[username] = public_key
+                    logger.info(f"Saved public key for {username}")
+
+                logger.info(f"{username} @ {client_address[0]}:{client_address[1]} connected.")
+
+                t = threading.Thread(target=self.client_link, args=(client_socket, stop_event))
+                self.clients[client_socket] = t, Client(username)
+                t.daemon = True
+                t.start()
+
+        except Exception as e:
+            logger.error(f"Error on server thread: {e}")
+            pass
+
+    def close(self):
+        self.send_to_all({"type": "server_close"})
+        for s in list(self.clients.keys()):
+            thread, client = self.clients[s]
+            s.close()
+            thread.join()
+
+        self.server_sock.close()
+        self.clients.clear()
+
+        with open(self.KEY_FILE, "w") as f:
+            json.dump(self.public_keys, f, indent=2)
+
+
+def main():
+    HOST = socket.gethostbyname(socket.gethostname())
+    PORT = 6777
+
+    logger.info(f"Setting up for hosting at {HOST}:{PORT}")
+
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(5)
+
+    server = Server(s)
+
+    # Server thread
+    stop_event = threading.Event()
+    t = threading.Thread(target=server.loop, args=(stop_event,))
+    t.daemon = True
+    t.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.info("Exiting...")
+        pass
+
+    stop_event.set()
+    server.close()
+
+
+if __name__ == "__main__":
+    main()
